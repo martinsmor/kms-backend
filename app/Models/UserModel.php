3d424e4ebf651cb9bf23098f1a9d8f7b0555acbf
ryhanabr <116617880+ryhanabr@users.noreply.github.com>
2022-11-02 23:07:07 +0700

    Ganti nama kolom unit_kerja

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -14,7 +14,7 @@ class UserModel extends Model
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = ['username','password','nama','role','unit_kerja_kode'];
+    protected $allowedFields    = ['username','password','nama','role','unit_kerja'];
 
     // Dates
     protected $useTimestamps = false;
